4ae7e6b2a46c0aad9b8c5437aca86166c8824514
DmitryZaburunov <d.zaburunov@icloud.com>
2026-01-12 22:49:52 +0300

    add WebDriverWait to searchPage

--- src/main/java/by/fixPrice/pages/SearchPage.java
+++ src/main/java/by/fixPrice/pages/SearchPage.java
@@ -3,15 +3,15 @@ package by.fixPrice.pages;
 import org.openqa.selenium.By;
 
 public class SearchPage extends HomePage {
-    private final String SEARCH_PAGE_URL = "https://fix-price.by/search";
+    private final String SEARCH_PAGE_URL = "https://fix-price.by/search?q=qwer&sort=sold";
 
-    private final String NOT_FOUND_TEXT = "//div[@class=\"page-content\"]//div[@class=\"title\"]";
+    private final By NOT_FOUND_TEXT = By.xpath("//div[@class='page-content']//div[@class='title']");
 
     public void open() {
         openPage(SEARCH_PAGE_URL);
     }
 
     public String getNotFoundText() {
-        return driver.findElement(By.xpath(NOT_FOUND_TEXT)).getText();
+        return waitForVisibleOfElement(NOT_FOUND_TEXT).getText();
     }
 }
