085ce3c2d89bccb36ab77f2be3f7e9bc140a478e
DmitryZaburunov <d.zaburunov@icloud.com>
2026-01-12 22:49:32 +0300

    add WebDriverWait to project

--- src/main/java/by/fixPrice/pages/HomePage.java
+++ src/main/java/by/fixPrice/pages/HomePage.java
@@ -3,15 +3,26 @@ package by.fixPrice.pages;
 import by.fixPrice.driver.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     private final String BASE_URL = "https://fix-price.by/";
-    private final String COOKIE_ALERT_ACCEPT_BTN = "//div[@class='cookies-bar']//button[@class='button config']";
+    private final By COOKIE_ALERT_ACCEPT_BTN = By.xpath("//div[@class='cookies-bar']//button[@class='button config']");
 
     public WebDriver driver;
+    public WebDriverWait wait;
 
     public HomePage() {
         this.driver = Driver.getDriver();
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public WebElement waitForVisibleOfElement(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     public void openPage(String url) {
@@ -19,6 +30,9 @@ public class HomePage {
     }
 
     public void acceptCookies() {
-        driver.findElement(By.xpath(COOKIE_ALERT_ACCEPT_BTN)).click();
+        waitForVisibleOfElement(COOKIE_ALERT_ACCEPT_BTN).click();
     }
+
+    //Login
+
 }
