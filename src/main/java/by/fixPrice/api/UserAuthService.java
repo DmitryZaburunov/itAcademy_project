cb786ad5ea53e7b887b6a7a6e0be6cbe5879fa0a
DmitryZaburunov <d.zaburunov@icloud.com>
2026-01-31 17:10:38 +0300

    Add auth tests and methods

---
+++ src/main/java/by/fixPrice/api/UserAuthService.java
@@ -0,0 +1,50 @@
+package by.fixPrice.api;
+
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class UserAuthService {
+    private final String USER_AUTH_URL = "https://api.fix-price.by/buyer/v2/auth/login";
+
+    private Response response;
+
+    private Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Accept", "application/json, text/plain, */*");
+        headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36");
+        headers.put("x-city", "14");
+        headers.put("X-Key", "Vm5vbkNQZzY0NVdGUlZWaFc1NFhGQT09OjoxWkNBMVhtNHBKQjRZRjhMZmZubE1BPT0=:e53ed7021182b35047cdb1c66ac9a050");
+        return headers;
+    }
+
+    private String getBody(String email, String password) {
+        return "{\"password\":\"" + password + "\",\"email\":\"" + email + "\"}";
+    }
+
+    public void doRequest(String email, String password) {
+        response =
+                given()
+                        .headers(getHeaders())
+                        .body(getBody(email, password))
+                        .when()
+                        .post(USER_AUTH_URL);
+    }
+
+    public int getResponseCode() {
+        return response.statusCode();
+    }
+
+    public String getResponseMessage(String path) {
+        return response.jsonPath().getString(path);
+    }
+
+    public boolean getIsUserConfirmed() {
+        return response.jsonPath().getBoolean("isUserConfirmed");
+    }
+
+}
