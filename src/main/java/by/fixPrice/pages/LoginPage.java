5284819b9eb3a8763d00ded6efee9788c6da368d
DmitryZaburunov <d.zaburunov@icloud.com>
2026-02-08 15:43:07 +0300

    Add logs for methods

--- src/main/java/by/fixPrice/pages/LoginPage.java
+++ src/main/java/by/fixPrice/pages/LoginPage.java
@@ -21,103 +21,118 @@ public class LoginPage extends HomePage {
     private final By LOGIN_FORM_INVALID_CREDENTIAL_ERROR = By.xpath("//div[@id='modal']/dialog[@class='wrapper modal-child']/div[@data-component='AuthWrapperError']/p");
 
     public void clickLoginFormToggleByPhone() {
+        logger.info("Clicking on login form toggle by phone");
         waitFVOE(LOGIN_FORM_TOGGLE_BY_PHONE).click();
     }
 
     public void clickLoginFormToggleByEmail() {
+        logger.info("Clicking on login form toggle by email");
         waitFVOE(LOGIN_FORM_TOGGLE_BY_EMAIL).click();
     }
 
     public void selectLoginForm(String formType) {
         if (formType.equals("Phone")) {
+            logger.info("Selecting login form to Phone");
             waitFVOE(LOGIN_FORM_TOGGLE_BY_PHONE).click();
         }
         if (formType.equals("Email")) {
+            logger.info("Selecting login form to email");
             waitFVOE(LOGIN_FORM_TOGGLE_BY_EMAIL).click();
         }
     }
 
     public void enterUserPhone(String phone) {
+        logger.info("Entering user phone number");
         waitFVOE(LOGIN_FORM_LOGIN_INPUT).sendKeys(phone);
     }
 
     public void enterUserEmail(String email) {
+        logger.info("Entering user email");
         waitFVOE(LOGIN_FORM_LOGIN_INPUT).sendKeys(email);
     }
 
     public void enterUserPassword(String password) {
+        logger.info("Entering user password");
         waitFVOE(LOGIN_FORM_PASSWORD_INPUT).sendKeys(password);
     }
 
     public void clickLoginFormSubmit() {
+        logger.info("Clicking on login form submit");
         waitFVOE(LOGIN_FORM_SUBMIT_BTN).click();
     }
 
     public void clickLoginFormForgotPasswordLink() {
+        logger.info("Clicking on login form forgot password link");
         waitFVOE(LOGIN_FORM_FORGOT_PASSWORD_LINK).click();
     }
 
     public void clickLoginFormRegisterLink() {
+        logger.info("Clicking on login form register link");
         waitFVOE(LOGIN_FORM_REGISTER_LINK).click();
     }
 
     public void submitCheckbox() {
+        logger.info("Getting checkbox status not checked");
         String checkboxCurrentClass = waitFVOE(LOGIN_FORM_CHECKBOX).getAttribute("class");
 
         if (checkboxCurrentClass.equals("checkbox-field")) {
+            logger.info("Submitting checkbox");
             waitFVOE(LOGIN_FORM_CHECKBOX).click();
         }
     }
 
     public String getProfileBtnText() {
+        logger.info("Getting profile button text");
         return waitFVOE(PROFILE_BTN_TEXT).getText();
     }
 
     public Boolean getCheckboxChecked() {
+        logger.info("Getting checkbox status checked");
         return waitFVOE(LOGIN_FORM_CHECKBOX).getAttribute("class").contains("checked");
     }
 
     public String getLoginFormTitle() {
+        logger.info("Getting login form title");
         return waitFVOE(LOGIN_FORM_TITLE).getText();
     }
 
     public String getLoginFormToggleByPhone() {
+        logger.info("Getting login form toggle by phone");
         return waitFVOE(LOGIN_FORM_TOGGLE_BY_PHONE).getText();
     }
 
     public String getLoginFormToggleByEmail() {
+        logger.info("Getting login form toggle by email");
         return waitFVOE(LOGIN_FORM_TOGGLE_BY_EMAIL).getText();
     }
 
     public String getLoginFormLoginLabelByPhone() {
+        logger.info("Getting login form login label");
         return waitFVOE(LOGIN_FORM_LOGIN_LABEL).getText();
     }
 
-    public WebElement getLoginFormLoginInputByPhone() {
-        return waitFVOE(LOGIN_FORM_LOGIN_INPUT);
-    }
-
     public String getLoginFormPasswordLabelByPhone() {
+        logger.info("Getting login form password label");
         return waitFVOE(LOGIN_FORM_PASSWORD_LABEL).getText();
     }
 
-    public WebElement getLoginFormPasswordInputByPhone() {
-        return waitFVOE(LOGIN_FORM_PASSWORD_INPUT);
-    }
-
     public WebElement getLoginFormSubmitButtonBy() {
+        logger.info("Getting login form submit button");
         return waitFVOE(LOGIN_FORM_SUBMIT_BTN);
     }
 
     public String getLoginErrorText() {
+        logger.info("Getting login error text");
         return waitFVOE(LOGIN_FORM_LOGIN_EMPTY_INPUT_ERROR).getText();
     }
 
     public String getPasswordErrorText() {
+        logger.info("Getting password error text");
         return waitFVOE(LOGIN_FORM_PASSWORD_EMPTY_INPUT_ERROR).getText();
     }
 
     public String getInvalidCredentialErrorText() {
+        logger.info("Getting invalid credential error text");
         return waitFVOE(LOGIN_FORM_INVALID_CREDENTIAL_ERROR).getText();
     }
 }
